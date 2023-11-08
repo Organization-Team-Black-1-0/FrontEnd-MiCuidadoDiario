5cb99e9a9822078787401ca1ffaa2935bf1cd064
BEMM12 <bemm1984@gmail.com>
2023-11-08 17:14:08 -0400

    ajustes cheklist section

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const menuItems = document.querySelectorAll('.menu-item');
-menuItems.forEach(function(item){
-    item.addEventListener('click', function(e) {
-        const currentItem = document.querySelector('.active');
-        currentItem.classList.remove('active');
-        e.target.classList.add('active');
-    });
+const menuItems = document.querySelectorAll(".menu-item");
+menuItems.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    const currentItem = document.querySelector(".active");
+    currentItem.classList.remove("active");
+    e.target.classList.add("active");
+  });
 });
-
